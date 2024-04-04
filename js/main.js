ab90c01f23d629bb9e22d4178cc98ef31d892227
StanislavTihanov <117075179+StanislavTihanov@users.noreply.github.com>
2024-04-04 17:50:21 +0300

    Add animation

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@
 //------------------------------------------------------------------------preloader
 
 
-
 //------------------------------------------------------------------------Меню-Бургер
 const burgerMenu = document.querySelector('.header__burger');
 const menuBody= document.querySelector('.menu');
@@ -489,36 +488,36 @@ if (menuLinks.length > 0) {
 
 
 //------------------------------------------------------------------------Animation
-//const animItems = document.querySelectorAll('._anim-items');
-//if (animItems.length > 0) {
-//  window.addEventListener('scroll', animOnScroll);
-//  function animOnScroll() {
-//    for (let index = 0; index < animItems.length; index++) {
-//        const animItem = animItems[index];
-//        const animItemHeight = animItem.offsetHeight;
-//        const animItemOffset = offset(animItem).top;
-//        const animStart = 5;
-//
-//        let animItemPoint = window.innerHeight - animItemHeight / animStart;
-//
-//        if (animItemHeight > window.innerHeight) {
-//          animItemPoint = window.innerHeight - window.innerHeight / animStart;
-//        }
-//
-//        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-//          animItem.classList.add('_action');
-//        } else {
-//          animItem.classList.remove('_action');
-//        }
-//    }
-//  }
-//  function offset(el) {
-//    const rect = el.getBoundingClientRect(),
-//    scrollLeft  = window.pageXOffset || document.documentElement.scrollLeft,
-//    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//    return {top: rect.top + scrollTop, left: rect.left + screenLeft}
-//  }
-//  animOnScroll();
-//}
+const animItems = document.querySelectorAll('._anim-items');
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 5;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+        if (animItemHeight > window.innerHeight) {
+          animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
+
+        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+          animItem.classList.add('_action');
+        } else {
+          animItem.classList.remove('_action');
+        }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+    scrollLeft  = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return {top: rect.top + scrollTop, left: rect.left + screenLeft}
+  }
+  animOnScroll();
+}
 //------------------------------------------------------------------------Animation
 
