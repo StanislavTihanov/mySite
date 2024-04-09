96e5fd1a3b471550b9fe80cd117569ec02abed10
StanislavTihanov <117075179+StanislavTihanov@users.noreply.github.com>
2024-04-09 09:01:55 +0300

    Добавил обработку персональных данных

--- server.php
+++ server.php
@@ -1,79 +1,25 @@
 <?php
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+	$phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
 
+	$to = "stas89tihanov@gmail.com"; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$from = $email;
+	$subject = "Заявка c сайта";
 
-$data = file_get_contents('php://input');
-//file_put_contents('1.json', $data);
+	
+	$msg="
+    Имя: $name /n
+    Фамилия: $surname /n
+    Телефон: $phone /n
+    Почта: $email /n
+    Текст: $text"; 	
+	mail($to, $subject, $msg, "From: $from ");
 
-$json = json_decode($data, true);
+?>
 
-
-if ($json['step0']['question'] && $json['step0']['answers']) {
-    
-} else {
-  $response = [
-    "status" => "error",
-    "message" => "Что-то пошло не так ..."
-  ];
-}
-
-if ($json['step1']['question'] && $json['step1']['answers']) {
-  
-} else {
-  $response = [
-    "status" => "error",
-    "message" => "Что-то пошло не так ..."
-  ];
-}
-
-if ($json['step2']['question'] && $json['step2']['answers']) {
-  
-} else {
-  $response = [
-    "status" => "error",
-    "message" => "Что-то пошло не так ..."
-  ];
-}
-
-if ($json['step3']['question'] && $json['step3']['answers']) {
-  
-} else {
-  $response = [
-    "status" => "error",
-    "message" => "Что-то пошло не так ..."
-  ];
-}
-
-$data_empty = false;
-
-foreach($json['step4'] as $item) {
-  if (!$item) $data_empty = true;
-}
-
-if (!$data_empty) {
-  
-    $response = [
-        "status" => "ok",
-        "message" => "Спасибо! Мы скоро с вами свяжемся"
-      ];
-
-} else {
-
-    if(!$json['step4']['name']) {
-        $error_message = "Введите имя";
-    } else if (!$json['step4']['phone']) {
-        $error_message = "Введите телефон";
-    } else if (!$json['step4']['email']) {
-        $error_message = "Введите email";
-    } else if (!$json['step4']['call']) {
-        $error_message = "Выберите пункт";
-    } else {
-        $error_message = "Что-то пошло не так ...";
-    }
-
-  $response = [
-    "status" => "error",
-    "message" => $error_message
-  ];
-}
-header("Content-Type: application/json; charset=utf-8");
-echo json_encode($response);
+<p>Привет, форма отправлена</p>
