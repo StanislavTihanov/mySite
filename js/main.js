8bf154f31fab2ccc8865427f3b529dd330bc5be1
StanislavTihanov <117075179+StanislavTihanov@users.noreply.github.com>
2024-12-09 11:28:09 +0300

    обновил отправку формы

--- js/main.js
+++ js/main.js
@@ -1,37 +1,5 @@
 "use strict"
 
-jQuery(document).ready(function () {
-    
-  $(".phone").mask("+7 (999) 999-99-99"); 
-  
-  jQuery('.send-form').click( function() {
-    var form = jQuery(this).closest('form');
-    
-    if ( form.valid() ) {
-      form.css('opacity','.5');
-      var actUrl = form.attr('action');
-
-      jQuery.ajax({
-        url: actUrl,
-        type: 'post',
-        dataType: 'html',
-        data: form.serialize(),
-        success: function(data) {
-          form.html(data);
-          form.css('opacity','1');
-                  form.find('.status').html('форма отправлена успешно');
-                  //$('#myModal').modal('show') // для бутстрапа
-        },
-        error:	 function() {
-            form.find('.status').html('серверная ошибка');
-        }
-      });
-    }
-  });
-
-
-});
-
 
 
 //------------------------------------------------------------------------Меню-Бургер
@@ -86,7 +54,114 @@ if (menuLinks.length > 0) {
 }
 //------------------------------------------------------------------------Прокрутка при клике
 
+//------------------------------------------------------------------------Обработка формы
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', formSend);
+ 
+  async function formSend(e) {
+    e.preventDefault();
+   
+    let error = formValidate(form);
+    let formData = new FormData(form);
+    formData.append('image', formImage.files[0]);
 
+    if (error === 0) {
+      form.classList.add('_sending');
+      let response = await fetch('sendmail.php', {
+        method: 'POST',
+        body: formData
+      });
+
+      if (response.ok) {
+        let result = await response.json();
+        alert(result.message);
+        formPreview.innerHTML = '';
+        form.reset();
+        form.classList.remove('_sending');
+      } else {
+        alert('Ошибка');
+        form.classList.remove('_sending');
+      }
+      
+    } else {
+      alert('Заполните обязательные поля')
+    }
+
+  }
+
+  function formValidate(form) {
+      let error = 0;
+      let formReq = document.querySelectorAll('._req');
+
+      for (let index = 0; index < formReq.length; index++) {
+        const input = formReq[index];
+        formRemoveError(input);
+
+        if (input.classList.contains('_email')) {
+            if (!emailTest(input)) {  // проверка на корректность email
+                formAddError(input);
+                error++;
+            }
+        } else if (input.getAttribute('type') === "checkbox" && input.checked === false) {
+          formAddError(input);
+          error++;
+        } else {
+           if (input.value === '') {
+             formAddError(input);
+             error++;
+           }
+        }
+      }
+      return error;
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+  }
+
+  function formRemoveError(input) {
+    input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+  }
+
+  // проверка email
+  function emailTest(input) {
+    return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(input.value); 
+  }
+
+  //получаем инпут file в переменную
+  const formImage = document.getElementById('formImage');
+  const formPreview = document.getElementById('formPreview');
+
+  formImage.addEventListener('change', () => {
+    uploadFile(formImage.files[0]);
+  });
+  
+  function uploadFile(file) {
+    if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+      alert('Только изображения');
+      formImage.value = '';
+      return;
+
+    }
+    if (file.size > 2 * 1024 * 1024) {
+      alert('Файл длжен быть менее 2 МБ');
+      return;
+    }
+    let reader = new FileReader();
+    reader.onload = function(e) {
+      formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
+    };
+    reader.onerror = function (e) {
+      alert('Ошибка');
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+//------------------------------------------------------------------------Обработка формы
 
 //------------------------------------------------------------------------Слайдер
 const slider = new Swiper('.swiper', {
@@ -118,7 +193,6 @@ new Swiper('.feedback__slider', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
   speed: 2000,
   breakpoints: {
     // when window width is >= 320px
@@ -242,3 +316,37 @@ document.addEventListener('keydown', function (e) {
 
 //------------------------------------------------------------------------popup
 
+
+//------------------------------------------------------------------------animate
+const animItems = document.querySelectorAll('._anim-items');
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 5;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+        if (animItemHeight > window.innerHeight) {
+          animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
+
+        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+          animItem.classList.add('_action');
+        } else {
+          animItem.classList.remove('_action');
+        }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+    scrollLeft  = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return {top: rect.top + scrollTop, left: rect.left + screenLeft}
+  }
+  animOnScroll();
+}
+//------------------------------------------------------------------------animate
